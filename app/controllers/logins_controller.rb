bc1f47ce5f7b105fae1e7a0b5e86df61c7a85aac
Andrea <andrealovrinovic@yahoo.com>
2019-06-25 12:55:30 +0200

    Added restrictions in controllers

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,27 @@
+class LoginsController < ApplicationController
+
+    skip_before_action :require_user, only: [:new, :create]
+
+    def new
+        
+    end
+
+    def create
+        student = Student.find_by(email: params[:logins][:email])
+        if student && student.authenticate(params[:logins][:password])
+            session[:student_id] = student.id
+            flash[:notice] = "You've successfully logged in."
+            redirect_to student
+        else
+            flash.now[:notice] = "Something went wrong with your login credentials."
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:student_id] = nil
+        flash[:notice] = "You've successfully logged out."
+        redirect_to root_path
+    end
+
+end
