ba5c3917e14e29a2a334821b5ed185467b4955b9
Andrea <andrealovrinovic@yahoo.com>
2019-06-25 09:32:40 +0200

    added student resources and working on flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'about', to: 'pages#about'
   get 'courses/new', to: 'courses#new'
+  
+  resources :students, except: [:destroy]
+
 end
